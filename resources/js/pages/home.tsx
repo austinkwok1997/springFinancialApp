086d6737b631201f37d118592292f85e91eba1db
kwok8 <austin.kwok@protonmail.com>
2025-06-09 22:08:07 -0700

    Add search/filter

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -1,8 +1,7 @@
 import Leaderboard from "@/components/leaderboard/leaderboard";
 import { type User } from "@/types";
 import AddUserButton from "@/components/addUser/addUserButton";
-import NameFilter from '@/components/nameFilter/nameFilter';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Input } from '@/components/ui/input';
 
 type InputEvent = React.ChangeEvent<HTMLInputElement>;
@@ -15,13 +14,17 @@ export default function Home({ users }: HomeProps) {
     const [userList, setUserList] = useState(users);
     const [filterTerm, setFilterTerm] = useState("");
 
+    useEffect(() => {
+        setUserList(users.filter(x => x.name.includes(filterTerm)))
+    }, [filterTerm])
+
     const handleChange = (e: InputEvent) => {
         setFilterTerm(e.target.value);
     }
 
     return (
         <>
-            <Input name="filter" type="text" onChange={handleChange} value={filterTerm} />
+            <Input name="filter" type="text" onChange={handleChange} value={filterTerm} placeholder="Search" />
             <Leaderboard userList={userList} setUserList={setUserList} />
             <AddUserButton />
         </>
