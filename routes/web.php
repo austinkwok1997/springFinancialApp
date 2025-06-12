6b6986fe69369441f0f4630bef01d30630ba9889
kwok8 <austin.kwok@protonmail.com>
2025-06-12 08:22:00 -0700

    Added brief description of endpoints

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,19 @@ use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
+// Renders home page
 Route::get('/', function () {
     $users = User::all();
     return Inertia::render('home', ["users" => $users]);
 });
 
+// Changes user's points
 Route::put('/points/{user}', [UserController::class, 'updatePoints']);
 
+// Adds user
 Route::post('/addUser', [UserController::class, 'addUser']);
+
+// Deletes user
 Route::delete('/deleteUser/{user}', [UserController::class, 'deleteUser']);
 
 Route::middleware(['auth', 'verified'])->group(function () {
